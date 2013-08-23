b85d28d39b4b8fbeb0a26d251ff019e03a47c74c
Xander <xander.v@crazydomains.com>
2013-08-23 10:13:17 +0800

    move CEC_TV into own file

---
+++ CEC_TV.cpp
@@ -0,0 +1,84 @@
+#ifndef CEC_TV_H__
+#define CEC_TV_H__
+
+#include "CEC_TV.h"
+
+void CEC_TV::OnReceive(int source, int dest, unsigned char* buffer, int count){
+    DbgPrint("%d -> %d (%d): ", source, dest, count);
+    if (count){
+        switch(buffer[0]){
+            case CEC_FEATURE_ABORT: DbgPrint("CEC_FEATURE_ABORT\r\n"); break;
+            case CEC_OTP_IMAGE_ON: DbgPrint("CEC_OTP_IMAGE_ON\r\n"); break;
+            //case CEC_TUNER_UP: DbgPrint("CEC_TUNER_UP\r\n"); break;
+            //case CEC_TUNER_DOWN: DbgPrint("CEC_TUNER_DOWN\r\n"); break;
+//                case CEC_TUNER_STATUS: DbgPrint("CEC_TUNER_STATUS\r\n"); break;
+//                case CEC_TUNER_STATUS_REQ: DbgPrint("CEC_TUNER_STATUS_REQ\r\n"); break;
+//                case CEC_OTR_REC_ON: DbgPrint("CEC_OTR_REC_ON\r\n"); break;
+//                case CEC_OTR_REC_STATUS: DbgPrint("CEC_OTR_REC_STATUS\r\n"); break;
+//                case CEC_OTR_REC_OFF: DbgPrint("CEC_OTR_REC_OFF\r\n"); break;
+            case CEC_OTP_TEXT_ON: DbgPrint("CEC_OTP_TEXT_ON\r\n"); break;
+            case CEC_OTR_REC_SCREEN: DbgPrint("CEC_OTR_REC_SCREEN\r\n"); break;
+//                case CEC_DECK_REQ_STATUS: DbgPrint("CEC_DECK_REQ_STATUS\r\n"); break;
+//                case CEC_DECK_STATUS: DbgPrint("CEC_DECK_STATUS\r\n"); break;
+//                case CEC_INFO_LANG: DbgPrint("CEC_INFO_LANG\r\n"); break;
+//                case CEC_TIMER_CLEAR_ANALOG: DbgPrint("CEC_TIMER_CLEAR_ANALOG\r\n"); break;
+//                case CEC_TIMER_SET_ANALOG: DbgPrint("CEC_TIMER_SET_ANALOG\r\n"); break;
+//                case CEC_TIMER_STATUS: DbgPrint("CEC_TIMER_STATUS\r\n"); break;
+            case CEC_STANDBY: DbgPrint("CEC_STANDBY\r\n"); break;
+            case CEC_DECK_PLAY: DbgPrint("CEC_DECK_PLAY\r\n"); break;
+            case CEC_DECK_CTRL: DbgPrint("CEC_DECK_CTRL\r\n"); break;
+//                case CEC_TIMER_STATUS_CLEARED: DbgPrint("CEC_TIMER_STATUS_CLEARED\r\n"); break;
+//                case CEC_MENU_UC_PRESSED: DbgPrint("CEC_MENU_UC_PRESSED\r\n"); break;
+            case CEC_OSD_REQ_OSD: DbgPrint("CEC_OSD_REQ_OSD\r\n"); break;
+            case CEC_OSD_SET_OSD: DbgPrint("CEC_OSD_SET_OSD\r\n"); break;
+            case CEC_OSD_SEND: DbgPrint("CEC_OSD_SEND\r\n"); break;
+//                case CEC_TIMER_SET_TITLE: DbgPrint("CEC_TIMER_SET_TITLE\r\n"); break;
+//                case CEC_AUDIO_MODE_REQ: DbgPrint("CEC_AUDIO_MODE_REQ\r\n"); break;
+//                case CEC_AUDIO_STATUS_REQ: DbgPrint("CEC_AUDIO_STATUS_REQ\r\n"); break;
+//                case CEC_AUDIO_MODE_SET: DbgPrint("CEC_AUDIO_MODE_SET\r\n"); break;
+//                case CEC_AUDIO_STATUS: DbgPrint("CEC_AUDIO_STATUS\r\n"); break;
+//                case CEC_Give_System_Audio_Mode_Status: DbgPrint("CEC_Give_System_Audio_Mode_Status\r\n"); break;
+//                case CEC_AUDIO_MODE: DbgPrint("CEC_AUDIO_MODE\r\n"); break;
+            case CEC_ROUTING_CHANGED: DbgPrint("CEC_ROUTING_CHANGED\r\n"); break;
+            case CEC_ROUTING_INFO: DbgPrint("CEC_ROUTING_INFO\r\n"); break;
+            case CEC_OTP_ACTIVE_SRC: DbgPrint("CEC_OTP_ACTIVE_SRC\r\n"); break;
+            case CEC_INFO_REQ_PHYS_ADDR: DbgPrint("CEC_INFO_REQ_PHYS_ADDR\r\n"); break;
+            case CEC_INFO_PHYS_ADDR: DbgPrint("CEC_INFO_PHYS_ADDR\r\n"); break;
+            case CEC_ROUTING_REQ_ACTIVE: DbgPrint("CEC_ROUTING_REQ_ACTIVE\r\n"); break;
+            case CEC_ROUTING_REQ_PATH: DbgPrint("CEC_ROUTING_REQ_PATH\r\n"); break;
+            case CEC_VENDOR_ID: DbgPrint("CEC_VENDOR_ID\r\n"); break;
+            case CEC_VENDOR_COMMAND: DbgPrint("CEC_VENDOR_COMMAND\r\n"); break;
+            case CEC_VENDOR_REMOTE_BDOWN: DbgPrint("CEC_VENDOR_REMOTE_BDOWN\r\n"); break;
+            case CEC_VENDOR_REMOTE_BUP: DbgPrint("CEC_VENDOR_REMOTE_BUP\r\n"); break;
+            case CEC_VENDOR_ID_REQ: DbgPrint("CEC_VENDOR_ID_REQ\r\n"); break;
+            case CEC_MENU_REQ: DbgPrint("CEC_MENU_REQ\r\n"); break;
+            case CEC_MENU_STATUS: DbgPrint("CEC_MENU_STATUS\r\n"); break;
+            case CEC_POWER_REQ_STATUS: DbgPrint("CEC_POWER_REQ_STATUS\r\n"); break;
+            case CEC_POWER_STATUS: DbgPrint("CEC_POWER_STATUS\r\n"); break;
+            case CEC_INFO_LANG_REQ: DbgPrint("CEC_INFO_LANG_REQ\r\n"); break;
+//                case CEC_TUNER_SELECT_ANALOG: DbgPrint("CEC_TUNER_SELECT_ANALOG\r\n"); break;
+//                case CEC_TUNER_SELECT_DIGITAL: DbgPrint("CEC_TUNER_SELECT_DIGITAL\r\n"); break;
+//                case CEC_TIMER_SET_DIGITAL: DbgPrint("CEC_TIMER_SET_DIGITAL\r\n"); break;
+//                case CEC_TIMER_CLEAR_DIGITAL: DbgPrint("CEC_TIMER_CLEAR_DIGITAL\r\n"); break;
+//                case CEC_AUDIO_RATE: DbgPrint("CEC_AUDIO_RATE\r\n"); break;
+            case CEC_ROUTING_INACTIVE: DbgPrint("CEC_ROUTING_INACTIVE\r\n"); break;
+            case CEC_INFO_VERSION: DbgPrint("CEC_INFO_VERSION\r\n"); break;
+            //case CEC_VENDOR_CEC_VERSION: DbgPrint("CEC_VENDOR_CEC_VERSION\r\n"); break;
+            case CEC_INFO_VERSION_REQ: DbgPrint("CEC_INFO_VERSION_REQ\r\n"); break;
+            //case CEC_VENDOR_CEC_VERSION_REQ: DbgPrint("CEC_VENDOR_CEC_VERSION_REQ\r\n"); break;
+            case CEC_VENDOR_COMMAND_ID: DbgPrint("CEC_VENDOR_COMMAND_ID\r\n"); break;
+            case CEC_TIMER_CLEAR_EXTERNAL: DbgPrint("CEC_TIMER_CLEAR_EXTERNAL\r\n"); break;
+            case CEC_TIMER_SET_EXTERNAL: DbgPrint("CEC_TIMER_SET_EXTERNAL\r\n"); break;
+            default: DbgPrint("unknown (%d)\r\n", buffer[0]); break;
+        }
+        if (count > 1) {
+            for (int i = 1; i < count; i++)
+            DbgPrint("%02X ", buffer[i]);
+            DbgPrint("\r\n");
+        } else {
+            DbgPrint("Ping \r\n");
+        }
+    }
+}
+
+#endif // CEC_TV_H__
---
+++ CEC_TV.h
@@ -0,0 +1,15 @@
+#include "CEC_Device.h"
+#include "CEC_Codes.h"
+
+class CEC_TV : public CEC_Device
+{
+public:
+  CEC_TV():CEC_Device(0){
+      Promiscuous = true; //Relay all line traffic
+      Initialize(CEC_LogicalDevice::CDT_TV);
+  };
+  virtual ~CEC_TV(){};
+
+protected:
+    virtual void OnReceive(int source, int dest, unsigned char* buffer, int count);
+};
